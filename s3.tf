1874be9a1287b025fe02318c52bfd6ff68355e15
Ben Chong <chiangyaw@gmail.com>
2024-03-12 15:55:42 +0800

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -8,7 +8,5 @@ resource "aws_s3_bucket" "prismaclouds3" {
   tags = {
     Name        = "Prisma Cloud"
     Environment = "Dev"
-    yor_name    = "prismaclouds3"
-    yor_trace   = "063bf935-805c-44e1-8385-9d00e27b01cb"
   }
 }
